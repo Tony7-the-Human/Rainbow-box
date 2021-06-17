b568b26d5f396692d2533c167f6417076b494b77
Антон Простяков <antonprostakov@MacBook-Air-Anton.local>
2021-06-17 21:58:09 +0300

    add rainbow game

---
+++ app.js
@@ -0,0 +1,31 @@
+const board = document.querySelector('#board')
+const colors = ['#de54a6', '#de5474', '#d054de', '#9254de', '#5c54de', '#5490de', '#54ded9', '#54de8e', '#b3de54', '#dec754', '#de6a54']
+const SQUARES_NUMBER = 400
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
